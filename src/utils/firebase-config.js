912057a6f1b9d300f0a6a2b36ade82034d6753b8
kevin <k.wafo@yahoo.fr>
2020-04-07 07:05:26 +0200

    Modification de ma key firebase

--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -3,14 +3,14 @@ import { initializeApp } from 'firebase';
 export const initFirebase = () => {
     // configuration de firebase à récupérer dans la console firebase...
     var firebaseConfig = {
-      apiKey: "AIzaSyCpLFZ0jeUrFIM0ITjepcAX9L-dJw9QI9E",
-      authDomain: "my-netflix-706ea.firebaseapp.com",
-      databaseURL: "https://my-netflix-706ea.firebaseio.com",
-      projectId: "my-netflix-706ea",
-      storageBucket: "my-netflix-706ea.appspot.com",
-      messagingSenderId: "711208598393",
-      appId: "1:711208598393:web:cbceaba0ab887cb63ccda8",
-      measurementId: "G-TEBWQFTKBR"
+      apiKey: "*******",
+      authDomain: "*******",
+      databaseURL: "*******",
+      projectId: "*******",
+      storageBucket: "*******",
+      messagingSenderId: "*******",
+      appId: "*******",
+      measurementId: "*******"
       };
 
       initializeApp(firebaseConfig);
